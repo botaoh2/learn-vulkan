a91c520bb639682d1a49fc645e891fe91372f510
Botao Han <hanbotao599@gmail.com>
2025-10-30 23:47:51 +1100

    Add glm

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 
+#include <fmt/format.h>
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <fmt/format.h>
+#include <glm/glm.hpp>
 
 #include <iostream>
 
 int main() {
-    int a = 10;
-    fmt::println("Hello world! a = {}", a);
+    glm::vec3 v{1, 2, 3};
+    fmt::println("v = {} {} {}", v.x, v.y, v.z);
 
     glfwInit();
 
@@ -19,10 +21,6 @@ int main() {
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
     std::cout << extensionCount << " extensions supported\n";
-
-    // glm::mat4 matrix;
-    // glm::vec4 vec;
-    // auto test = matrix * vec;
 
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
